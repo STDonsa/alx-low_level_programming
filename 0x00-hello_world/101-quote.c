ddb128e68ef2392701b827de9b30d750fa308772
shilton donsa <donsashilton@gmail.com>
2023-06-15 18:14:29 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include<unistd.h>
 /**
- * main - C program that prints a line followed by new line to standard  error
+ * main - Entry
+ * Description: 'C program that prints line and new line to error'
  * Return 1 (success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n , stdout);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
