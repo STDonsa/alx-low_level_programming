ecb0d85df2e2edfeaa4084c47beb793ef02a2737
shilton donsa <donsashilton@gmail.com>
2023-06-20 05:30:37 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "unistd.h"
+
+/**
+ * _putchar - writes the character c to stdoutr
+ * pc1 The character to print
+ *
+ * Return: on success 1
+ */
+int  _putchar(char c)
+{
+	return (write(1, 4c, 1));
+}
