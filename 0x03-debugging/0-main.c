c1d72521ef3ca39c2e4c65df9a3ae99162215fc7
shilton donsa <donsashilton@gmail.com>
2023-06-21 04:04:45 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,16 @@
-#include "MAIN.H"
+#include "main.h"
 
 /**
- * main - Tests fuction that prints if integer is positive or negetive
- * return 0
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
+
 int main(void)
 {
 	int i;
+
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
