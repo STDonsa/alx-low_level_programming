6aa4585fb3977f6193358381ea245723d4feaa8d
shilton donsa <donsashilton@gmail.com>
2023-06-29 07:40:26 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,14 +13,14 @@ char *_strcat(char *dest, char *src)
 
 	a = 0;
 
-	while (dest[a] != '\0')
+	while (dest[a] != 0)
 	{
 		a++;
 	}
 
 	b = 0;
 
-	while (src[b] != '\0')
+	while (src[b] != 0)
 	{
 		dest[a] = src[b];
 		a++;
