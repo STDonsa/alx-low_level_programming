7a4d7a7298dd1640123bf5569b10461a64699133
shilton donsa <donsashilton@gmail.com>
2023-07-10 06:57:39 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * str_concat - get ends of input and add together for size
+ * str_concat - program gets ends of input and add together for size
  * @s1: input one to concat
  * @s2: input two to concat
  * Return: concat of s1 and s2
@@ -9,31 +9,33 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *conct;
-	int s, td;
+	int s, ci;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-		s = td = 0;
+
+	s = ci = 0;
 	while (s1[s] != '\0')
 		s++;
-	while (s2[td] != '\0')
-		td++;
-	conct = malloc(sizeof(char) * (s + td + 1));
+	while (s2[ci] != '\0')
+		ci++;
+	conct = malloc(sizeof(char) * (s + ci + 1));
 
 	if (conct == NULL)
 		return (NULL);
-	s = td = 0;
+	s = ci = 0;
 	while (s1[s] != '\0')
 	{
 		conct[s] = s1[s];
 		s++;
 	}
-	while (s2[td] != '\0')
+
+	while (s2[ci] != '\0')
 	{
-		conct[s] = s2[td];
-		s++, td++;
+		conct[s] = s2[ci];
+		s++, ci++;
 	}
 	conct[s] = '\0';
 	return (conct);
